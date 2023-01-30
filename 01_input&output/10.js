89abdb2321543b461b2b0557dbec7c2e444affca
youngyounkim <rladuddbs789@gmail.com>
2023-01-30 22:23:50 +0900

    feat: 2일차 입출력

--- 01_input&output/10.js
+++ 01_input&output/10.js
@@ -14,11 +14,11 @@
 const fs = require('fs');
 const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
 
-const A = input[0]
-const B = input[1]
-const C = input[2]
+const A = parseInt(input[0]);
+const B = parseInt(input[1]);
+const C = parseInt(input[2]);
 
-console.log((A+B)%C)
-console.log(((A%C)+(B%C))%C)
-console.log((A*B)%C)
-console.log(((A%C)*(B%C))%C)
+console.log((A+B)%C);
+console.log(((A%C)+(B%C))%C);
+console.log((A*B)%C);
+console.log(((A%C)*(B%C))%C);
