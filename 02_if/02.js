0197edaebd021c37835fb7011076fd370115a3d6
youngyounkim <rladuddbs789@gmail.com>
2023-02-01 22:43:27 +0900

    feat: 3일차 조건문

---
+++ 02_if/02.js
@@ -0,0 +1,24 @@
+// 문제
+// 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+
+// 입력
+// 첫째 줄에 시험 점수가 주어진다. 시험 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+
+// 출력
+// 시험 성적을 출력한다.
+
+
+const fs = require('fs');
+const input = fs.readFileSync("/dev/stdin").toString().trim();
+
+if(input >= 90){
+    console.log("A")
+} else if (input >= 80) {
+    console.log("B")
+} else if (input >= 70) {
+    console.log("C")
+} else if (input >= 60) {
+    console.log("D")
+} else {
+    console.log("F")
+}
